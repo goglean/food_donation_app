7b204ab7d4c503de08ca42f860e30a5198132ead
skajal1309 <skajal1309@gmail.com>
2022-01-08 21:25:00 +0530

    added donate items page

---
+++ lib/screens/donateItemsPage.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:food_donating_app/forms/donateItemsForm.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DonateItemPage extends StatefulWidget {
+  const DonateItemPage({Key? key}) : super(key: key);
+
+  @override
+  _DonateItemPageState createState() => _DonateItemPageState();
+}
+
+class _DonateItemPageState extends State<DonateItemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_rounded,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Text(
+          "Step 1: Donation Items",
+          style: GoogleFonts.roboto(
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      ),
+      body:DonateItemsForm(),
+    );
+  }
+}
--- lib/screens/donatePage.dart
+++ lib/screens/donatePage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_donating_app/screens/donateItemsPage.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class DonatePage extends StatefulWidget {
-  const DonatePage({ Key? key }) : super(key: key);
+  const DonatePage({Key? key}) : super(key: key);
 
   @override
   _DonatePageState createState() => _DonatePageState();
@@ -10,7 +13,80 @@ class _DonatePageState extends State<DonatePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: Column(
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+              Container(
+                //color: Colors.black,
+                height: MediaQuery.of(context).size.height * 0.06,
+                child: Text(
+                  "Welcome!",
+                  textAlign: TextAlign.left,
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w800,
+                    fontSize: MediaQuery.of(context).size.height * 0.05,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.2,
+                width: MediaQuery.of(context).size.width * 0.45,
+                color: Colors.grey[400],
+              )
+            ],
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.25,
+              //color: Colors.red,
+              child: Text(
+                "By donating food, we are not feeding people, not landfills, supporting local communities, and saving all the resourcess that went into producing that food from going to waste. \n\n To make donation you will need to list specific details of what you have available to be rescued.",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.w400,
+                  fontSize: MediaQuery.of(context).size.height * 0.025,
+                  color: Colors.black,
+                ),
+              )),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.height * 0.06,
+            //color: Colors.red,
+            child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(color: Theme.of(context).primaryColor)),
+                color: Theme.of(context).primaryColor,
+                onPressed: () => setState(() {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DonateItemPage()));
+                    }),
+                child: Text(
+                  "Get Started",
+                  textAlign: TextAlign.left,
+                  style: GoogleFonts.roboto(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: MediaQuery.of(context).size.height * 0.04),
+                )),
+          )
+        ],
+      ),
     );
   }
 }
