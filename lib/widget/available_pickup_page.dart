3cc63f292ead265399a2357f6fb0baed1032a388
Ashwani Kumar <ashwanikumar97531@gmail.com>
2022-01-18 22:57:05 +0530

    map thing completed

--- lib/widget/available_pickup_page.dart
+++ lib/widget/available_pickup_page.dart
@@ -23,7 +23,7 @@ class _AvaiablePickupsState extends State<AvaiablePickups> {
   bool charityClicked = false;
   bool recievedCharityData = false;
   Charity? curCharity = null;
-  double distanceBetweenMarker = 0;
+  String distanceBetweenMarker = '0';
 
   @override
   Widget build(BuildContext context) {
@@ -67,11 +67,13 @@ class _AvaiablePickupsState extends State<AvaiablePickups> {
 
           Charity charityData =
               await MapService().getCharityDataFromFirebase(charity[i].uniId);
-          distanceBetweenMarker = Geolocator.distanceBetween(
-              double.parse(charityData.posLat),
-              double.parse(charityData.posLng),
-              double.parse(curRes.posLat),
-              double.parse(curRes.posLng));
+          distanceBetweenMarker = (Geolocator.distanceBetween(
+                      double.parse(charityData.posLat),
+                      double.parse(charityData.posLng),
+                      double.parse(curRes.posLat),
+                      double.parse(curRes.posLng)) *
+                  0.000621371)
+              .toStringAsFixed(3);
 
           print(charityData.uniId);
           setState(() {
@@ -82,6 +84,61 @@ class _AvaiablePickupsState extends State<AvaiablePickups> {
           });
         },
       ));
+    }
+
+    void openDialogue() {
+      // print('object');
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                  height: 150.0,
+                  width: 150.0,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Thank you for claiming this pickup!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.orange[700],
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'You can start your pickup upto one hour before time begins.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 16),
+              RaisedButton(
+                textColor: Colors.white,
+                color: Colors.orange[600],
+                child: Text(
+                  'OKAY',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+                onPressed: () {
+                  print('object');
+                },
+              ),
+            ],
+          ),
+        ),
+      );
     }
 
     return Column(
@@ -112,28 +169,181 @@ class _AvaiablePickupsState extends State<AvaiablePickups> {
                                   const BoxDecoration(color: Colors.black),
                             ),
                           ),
+
+                          // location details
                           ListTile(
-                            leading: Icon(Icons.access_time),
+                            visualDensity:
+                                VisualDensity(horizontal: 0, vertical: -4),
+                            horizontalTitleGap: 0,
+                            leading: Icon(
+                              Icons.access_time,
+                              size: 20,
+                            ),
                             title: Text(
-                                '${curCharity?.openTime} - ${curCharity?.closeTime}'),
+                              '${curCharity?.openTime} - ${curCharity?.closeTime}',
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
                           ),
                           ListTile(
-                            leading: Icon(Icons.location_on),
-                            title: Text(distanceBetweenMarker.toString()),
+                            visualDensity:
+                                VisualDensity(horizontal: 0, vertical: -4),
+                            horizontalTitleGap: 0,
+                            leading: Icon(
+                              Icons.location_on,
+                              size: 20,
+                            ),
+                            title: RichText(
+                              text: TextSpan(
+                                  style: DefaultTextStyle.of(context).style,
+                                  children: [
+                                    TextSpan(
+                                      text:
+                                          '${distanceBetweenMarker} miles from ',
+                                    ),
+                                    TextSpan(
+                                      text: curRes.name,
+                                      style:
+                                          TextStyle(color: Colors.orange[800]),
+                                    ),
+                                    TextSpan(
+                                      text: ' to ',
+                                    ),
+                                    TextSpan(
+                                      text: curCharity!.name,
+                                      style: TextStyle(
+                                        color: Colors.orange[300],
+                                      ),
+                                    )
+                                  ]),
+                            ),
                           ),
-                          // Row(
-                          //   children: [
-                          //     Container(
-                          //       child: Icon(Icons.access_alarm),
-                          //     ),
-                          //     // Text(
-                          //     //     '${curCharity?.openTime} - ${curCharity?.closeTime}')
-                          //   ],
-                          // ),
-                          Container(
-                            height: double.infinity,
-                            color: Colors.orange[50],
+                          SizedBox(
+                            height: 2,
+                            child: const DecoratedBox(
+                              decoration:
+                                  const BoxDecoration(color: Colors.grey),
+                            ),
                           ),
+
+                          // pickup and dropoff details
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.orange[800],
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                                  radius: 25,
+                                ),
+                              ),
+                              title: Text(
+                                'Pickup from ${curRes.name} at address dfawf agafd gg aga g zsfgs g zdsf g zdf',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8, bottom: 8),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.orange[300],
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      'https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                                  radius: 25,
+                                ),
+                              ),
+                              title: Text(
+                                'Drop off to ${curCharity!.name} at address',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 2,
+                            child: const DecoratedBox(
+                              decoration:
+                                  const BoxDecoration(color: Colors.grey),
+                            ),
+                          ),
+
+                          // What food will i pick
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(24, 8, 0, 0),
+                            child: Text(
+                              'What food will I pick',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(24, 8, 0, 8),
+                            child: Text('food 1\nfood2'),
+                          ),
+                          SizedBox(
+                            height: 2,
+                            child: const DecoratedBox(
+                              decoration:
+                                  const BoxDecoration(color: Colors.grey),
+                            ),
+                          ),
+
+                          // What you need to know
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(24, 8, 0, 0),
+                            child: Text(
+                              'What you need to know',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(24, 8, 0, 8),
+                            child: Text('food 1\nfood2'),
+                          ),
+                          SizedBox(
+                            height: 2,
+                            child: const DecoratedBox(
+                              decoration:
+                                  const BoxDecoration(color: Colors.grey),
+                            ),
+                          ),
+
+                          //Food donor pickup details
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(24, 8, 0, 0),
+                            child: Text(
+                              "Food Donor's Pick-Up Details",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(24, 8, 0, 8),
+                            child: Text(
+                                'Come to the back door. Call us when you are near'),
+                          ),
+                          SizedBox(
+                            height: 2,
+                            child: const DecoratedBox(
+                              decoration:
+                                  const BoxDecoration(color: Colors.grey),
+                            ),
+                          ),
+
+                          // CLaim Button
                           Padding(
                             padding: const EdgeInsets.fromLTRB(24, 16, 24, 8),
                             child: RaisedButton(
@@ -148,7 +358,9 @@ class _AvaiablePickupsState extends State<AvaiablePickups> {
                               shape: new RoundedRectangleBorder(
                                 borderRadius: new BorderRadius.circular(30.0),
                               ),
-                              onPressed: () {},
+                              onPressed: () {
+                                openDialogue();
+                              },
                             ),
                           )
                         ],
