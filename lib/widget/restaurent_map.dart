b61fd4e42338422034c66dd0dd371fd0e2e51151
Ashwani Kumar <ashwanikumar97531@gmail.com>
2022-02-13 11:02:03 +0530

    made history and correct time things

--- lib/widget/restaurent_map.dart
+++ lib/widget/restaurent_map.dart
@@ -256,7 +256,7 @@ class _RestaurentMapState extends State<RestaurentMap> {
                   double.parse(restaurent[i].lng),
                 ) >
                 20 &&
-            TimeCheck().getRestaurantOpenStatus(
+            !TimeCheck().getRestaurantOpenStatus(
                 restaurent[i].startTime, restaurent[i].startTime)) {
           continue;
         }
