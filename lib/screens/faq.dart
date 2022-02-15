77bfcfb19a2b8037e9039e7bfe745c4987551fae
Prajeeth347 <prajeeth347@gmail.com>
2022-02-15 14:07:51 +0530

    FAQ page

--- lib/screens/faq.dart
+++ lib/screens/faq.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FaqPage extends StatefulWidget {
   const FaqPage({Key? key}) : super(key: key);
@@ -8,6 +11,8 @@ class FaqPage extends StatefulWidget {
 }
 
 class _FaqPageState extends State<FaqPage> {
+  final Stream<QuerySnapshot> _collegesstream =
+      FirebaseFirestore.instance.collection('FAQ').snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,15 +20,65 @@ class _FaqPageState extends State<FaqPage> {
           backgroundColor: Theme.of(context).primaryColor,
           foregroundColor: Colors.white,
           leading: IconButton(
-            icon: Icon(Icons.arrow_back),
+            icon: Icon(Icons.arrow_back,color: Colors.black,),
             onPressed: () {
               Navigator.pop(context);
             },
           ),
-          title: Text("FAQ"),
+          title: Text("Help Center"),
         ),
-        body: Container(
-          color: Theme.of(context).scaffoldBackgroundColor,
-        ));
-  }
+        body: StreamBuilder<QuerySnapshot>(
+        stream: _collegesstream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Something went wrong'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(18),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text('FAQs',style: GoogleFonts.roboto(
+                    color: Colors.black,
+                    fontSize: 22
+                  ),)),
+              ),
+              Expanded(
+                child: ListView(
+                  children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return Column(
+                      children: [
+                        Container(
+                          color: Colors.white,
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          child: ListTile(
+                            title: Text(
+                              data['Question'],
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            subtitle: Text(
+                              data['Answer'],
+                              style: TextStyle(color: Colors.black54),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        )
+                      ],
+                    );
+                  }).toList()),
+              ),
+            ],
+          );
+        }
+          )
+        );
+}
 }
